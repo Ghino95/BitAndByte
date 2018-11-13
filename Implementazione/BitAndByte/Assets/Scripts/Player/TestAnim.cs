d043801d9219a00d2b9aa458ce52450cdf3d601a
Gianluigi Oliva <gianluigi.oliva@mail.polimi.it>
2018-11-13 11:08:12 +0100

    test animazioni

---
+++ Implementazione/BitAndByte/Assets/Scripts/Player/TestAnim.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestAnim : MonoBehaviour {
+
+
+    private Animator anim;
+    private Rigidbody2D rig;
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+        rig = GetComponent<Rigidbody2D>();
+    }
+
+   
+	
+	// Update is called once per frame
+	void Update () {
+
+        Vector2 velocity = rig.velocity;
+
+        if(velocity.x > 0){
+            anim.SetInteger("Direction", 1);
+        } else if(velocity.x < 0){
+            anim.SetInteger("Direction", -1);
+        }
+        else{
+            anim.SetInteger("Direction", 0);
+        }
+		
+	}
+}
