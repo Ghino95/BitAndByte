a6e733ddad2f8896b85bc213aa2944000fa48265
Gianluigi Oliva <gianluigi.oliva@mail.polimi.it>
2018-11-17 12:03:28 +0100

    livello 4-3

---
+++ Implementazione/BitAndByte/Assets/Scripts/Object/Wall.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wall : InferfaceEffect{
+
+    private Animator anim;
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+        
+    }
+
+
+
+    public override void PerformEffect(GameObject oggetto)
+    {
+        anim.SetTrigger("Activate");
+    }
+
+
+}
