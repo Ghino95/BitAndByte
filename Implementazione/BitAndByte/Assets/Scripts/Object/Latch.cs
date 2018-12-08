1a5a86b65597e68ee022baa2fbb05766efefa41c
Gianluigi Oliva <gianluigi.oliva@mail.polimi.it>
2018-12-08 19:23:54 +0100

    grafica

--- Implementazione/BitAndByte/Assets/Scripts/Object/Latch.cs
+++ Implementazione/BitAndByte/Assets/Scripts/Object/Latch.cs
@@ -6,24 +6,14 @@ public class Latch : MonoBehaviour
 {
 
     public InferfaceEffect taget;
-    private Animator anim;
-
-
-    private void Awake()
-    {
-        anim = GetComponent<Animator>();
-    }
-
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        anim.SetInteger("Status", 1);
         taget.PerformEffect(null);
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
-        anim.SetInteger("Status", 0);
         taget.DisableEffect(null);
     }
 }
