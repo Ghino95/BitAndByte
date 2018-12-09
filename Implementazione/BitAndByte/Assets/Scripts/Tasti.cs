ebc2c18908b9f3d9fc41074c8e03acaf4df09142
Gianluigi Oliva <gianluigi.oliva@mail.polimi.it>
2018-12-09 14:47:31 +0100

    bug

---
+++ Implementazione/BitAndByte/Assets/Scripts/Tasti.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class Tasti : ScriptableObject {
+
+    public List<CustomDictionary> Joypad;
+    public List<CustomDictionary> Keyboard;
+
+
+    public Sprite GetImageButtonJoypad(string key)
+    {
+        foreach (CustomDictionary temp in Joypad)
+        {
+            if (temp.key == key)
+            {
+                return temp.Image;
+            }
+        }
+        return null;
+    }
+
+    public Sprite GetImageButtonKeyboard(string key)
+    {
+        foreach (CustomDictionary temp in Keyboard)
+        {
+            if (temp.key == key)
+            {
+                return temp.Image;
+            }
+        }
+        return null;
+    }
+
+
+}
+
+[System.Serializable]
+public class CustomDictionary
+{
+    public string key;
+    public Sprite Image;
+
+}
