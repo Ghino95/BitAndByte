e623119a973fc481b519d8ea72192c0b33af9d8d
Gianluigi Oliva <gianluigi.oliva@mail.polimi.it>
2018-12-16 10:31:41 +0100

    joypad image

---
+++ Implementazione/BitAndByte/Assets/Scripts/MainMenu/ButtonHUD.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonHUD : MonoBehaviour {
+
+    public Tasti ImagesKey;
+    private Image Image;
+
+    private void Awake()
+    {
+        Image = GetComponent<Image>();
+        if (Input.GetJoystickNames().Length > 0)
+        {
+            Image.sprite = ImagesKey.GetImageButtonJoypad(name);
+        }
+        else
+        {
+            Image.sprite = ImagesKey.GetImageButtonKeyboard(name);
+        }
+    }
+
+
+}
