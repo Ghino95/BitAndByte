69b06c44f0bda30d3c7698614ef6de33542a6cbe
Gianluigi Oliva <gianluigi.oliva@mail.polimi.it>
2018-12-24 14:27:19 +0100

    inizio 3-2

---
+++ Implementazione/BitAndByte/Assets/Scripts/Object/EnableLight.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnableLight : InferfaceEffect {
+
+    private Light Luce;
+
+    private void Awake()
+    {
+        Luce = GetComponent<Light>();
+        Luce.enabled = false;
+    }
+
+    public override void PerformEffect(GameObject oggetto)
+    {
+        Luce.enabled = true;
+    }
+
+}
