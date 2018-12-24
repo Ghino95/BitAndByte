b43e6fc39d609fc97f50a60309ad5e080a3eaf5b
Gianluigi Oliva <gianluigi.oliva@mail.polimi.it>
2018-12-24 17:25:02 +0100

    luce

---
+++ Implementazione/BitAndByte/Assets/Scripts/Object/LightIntermittenza.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightIntermittenza : MonoBehaviour {
+
+    private Light Luce;
+
+    private void Awake()
+    {
+        Luce = GetComponent<Light>();
+        StartCoroutine(Intermittenza());
+    }
+
+    private IEnumerator Intermittenza()
+    {
+        while (true)
+        {
+            Luce.enabled = true;
+            yield return new WaitForSeconds(Random.Range(1f,2.5f));
+            Luce.enabled = false;
+            yield return new WaitForSeconds(Random.Range(0.2f, 1f));
+            yield return null;
+        }
+    }
+
+
+}
